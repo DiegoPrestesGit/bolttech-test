301d30f1d510d2b1717b8b4b3a53c08c005ef245
Diego Prestes <diegoprestes.git@hotmail.com>
2022-07-24 23:25:19 -0300

    feat: product component

--- frontend/src/components/project-styles.js
+++ frontend/src/components/project-styles.js
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.button`
   background-color: inherit;
   color: var(--white);
   font-size: 18px;
+  padding: 2px 4px 2px 4px;
+  border: 0.5px solid;
+  border-radius: 6px;
+  color: var(--white);
+  font-size: 16px;
+  margin-bottom: 5px;
+
+  :hover {
+    cursor: pointer;
+    transition-duration: 0.2s;
+    background-color: var(--dark-purple);
+    font-weight: bold;
+  }
 `;
